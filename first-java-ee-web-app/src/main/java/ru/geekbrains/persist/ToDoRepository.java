8e4d28fec9b79fe9f51ca26d948bd99d90ac447b
Johnny3kk <some@mail.ru>
2020-11-06 17:09:04 +0300

    Lesson 3 code in work, still

--- first-java-ee-web-app/src/main/java/ru/geekbrains/persist/ToDoRepository.java
+++ first-java-ee-web-app/src/main/java/ru/geekbrains/persist/ToDoRepository.java
@@ -1,14 +1,17 @@
 package ru.geekbrains.persist;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class ToDoRepository {
 
     private final Connection conn;
 
-    public ToDoRepository(Connection conn) {
+    public ToDoRepository(Connection conn) throws SQLException {
         this.conn = conn;
+        createTableIfNotExists(conn);
     }
 
     public void insert(ToDo toDo) throws SQLException {
@@ -41,5 +44,27 @@ public class ToDoRepository {
             }
         }
         return new ToDo(-1L, "", null);
+    }
+
+    public List<ToDo> findAll() throws SQLException {
+        List<ToDo> res = new ArrayList<>();
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery( "select id, description, targetDate from todos;");
+
+            while (rs.next()) {
+                res.add(new ToDo(rs.getLong(1), rs.getString(2), rs.getDate(3, Calendar.getInstance()).toLocalDate()));
+            }
+        }
+        return res;
+    }
+
+    private void createTableIfNotExists(Connection conn) throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeQuery( "create table if not exists todos (\n" +
+                    "\tid int auto_increment primary key, \n" +
+                    "description varchar(25), \n" +
+                    "targetDate date \n" +
+                    ");");
+        }
     }
 }
