9db93524963c57c993f43f4614985f55e376f9b5
Johnny3kk <some@mail.ru>
2020-11-28 00:37:31 +0300

    2 - shopping cart with ejb

--- jsf-web-app/src/main/java/ru/geekbrains/controller/CartControllerEjb.java
+++ jsf-web-app/src/main/java/ru/geekbrains/controller/CartControllerEjb.java
@@ -2,11 +2,14 @@ package ru.geekbrains.controller;
 
 import ru.geekbrains.service.CartService;
 import ru.geekbrains.service.ToDoRepr;
+import ru.geekbrains.shop.persist.Product;
+import ru.geekbrains.shop.service.ProductRepr;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @SessionScoped
@@ -15,7 +18,20 @@ public class CartControllerEjb implements Serializable {
     @EJB
     private CartService cartService;
 
-    public void add(ToDoRepr todo) {
-
+    public String goChecking() {
+        return "cart.xhtml?faces-redirect=true";
     }
+
+    public void add(ProductRepr productRepr) {
+        cartService.add(productRepr);
+    }
+
+    public void remove(ProductRepr productRepr) {
+        cartService.remove(productRepr);
+    }
+
+    public List<ProductRepr> getAll() {
+        return cartService.getCart();
+    }
+
 }
--- jsf-web-app/src/main/java/ru/geekbrains/service/CartService.java
+++ jsf-web-app/src/main/java/ru/geekbrains/service/CartService.java
@@ -1,18 +1,32 @@
 package ru.geekbrains.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.geekbrains.shop.persist.Product;
+import ru.geekbrains.shop.service.ProductRepr;
+
 import javax.ejb.Stateful;
 import java.io.Serializable;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateful
 public class CartService implements Serializable {
 
-    public List<ToDoRepr> getAll() {
-        return Collections.emptyList();
+    private static final Logger logger = LoggerFactory.getLogger(CartService.class);
+
+    private List<ProductRepr> cart = new ArrayList<ProductRepr>();
+
+    public void add(ProductRepr productRepr) {
+        this.cart.add(productRepr);
     }
 
-    public void add(ToDoRepr todo) {
+    public void remove(ProductRepr productRepr) {
+        int index = this.cart.indexOf(productRepr);
+        cart.remove(index);
+    }
 
+    public List<ProductRepr> getCart() {
+        return cart;
     }
 }
